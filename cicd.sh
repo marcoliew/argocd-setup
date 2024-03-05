8b3293d37ab92660b5aec5244451bc2ed76731b3
Marco Liao <marco.liao@sydney.edu.au>
2024-03-05 11:04:57 +1100

    fix

--- cicd.sh
+++ cicd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 kubectl apply -f ./argocd-cicd-setup
+kubectl apply -f ./argocd-cicd-setup/dev/namespace.yaml
+kubectl apply -f ./argocd-cicd-setup/dev/
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 $SHELL
