24660fea26842d13077d5cfebffbb78f49640caa
Marco Liao <marco.liao@sydney.edu.au>
2024-03-06 11:14:25 +1100

    s

--- argo-install/manifest/install.sh
+++ argo-install/manifest/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 kubectl create namespace argocd
 kubectl apply -f https://github.com/argoproj/argo-cd/blob/master/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/manifests/install.yaml
 $SHELL
