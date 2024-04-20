fa3dd856fe6e006017472f446fc9b4fda451594b
Marco Liao <marco.liao@sydney.edu.au>
2024-04-20 17:41:03 +1000

    s

--- argo-install/terraform/argocd.tf
+++ argo-install/terraform/argocd.tf
@@ -1,6 +1,11 @@
 # helm repo add argo https://argoproj.github.io/argo-helm
 # helm repo update
 # helm install argocd -n argocd --create-namespace argo/argo-cd --version 3.35.4 -f terraform/values/argocd.yaml
+
+# Install manually
+# helm repo add argo https://argoproj.github.io/argo-helm
+# helm repo update
+# helm install argocd --namespace argocd --create-namespace --version 5.46.8 --values terraform/values/argocd.yaml argo/argo-cd
 resource "helm_release" "argocd" {
   name = "argocd"
 
@@ -8,7 +13,8 @@ resource "helm_release" "argocd" {
   chart            = "argo-cd"
   namespace        = "argocd"
   create_namespace = true
-  version          = "3.35.4"
+  version          = "5.46.8" #"3.35.4"
+  timeout          = 600
 
   values = [file("values/argocd.yaml")]
 }
